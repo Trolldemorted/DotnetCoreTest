e18587cbe34bbb5425ba16949bb14018e3c267e1
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2018-07-06 02:47:28 +0200

    initial commit

---
+++ DotnetCoreTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var Init = Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+            });
+            var TaskB = Task.Run(async () =>
+            {
+                await Init;
+                Console.WriteLine("B finished waiting");
+                await Task.Delay(5000);
+            });
+            var TaskC = Task.Run(async () =>
+            {
+                await Init;
+                Console.WriteLine("C finished waiting");
+                while (true) { }
+            });
+            Console.ReadKey();            
+        }
+    }
+}
